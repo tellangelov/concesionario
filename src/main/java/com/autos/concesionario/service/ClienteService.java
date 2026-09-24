4c00cac012ba902aafa83c30d43ab2dd09966a77
tellangelov <tell.angelov@gmail.com>
2026-09-24 17:01:54 -0500

    Update ClienteService.java

--- src/main/java/com/autos/concesionario/service/ClienteService.java
+++ src/main/java/com/autos/concesionario/service/ClienteService.java
@@ -1,7 +1,11 @@
 package com.autos.concesionario.service;
+import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.autos.concesionario.model.Cliente;
 import com.autos.concesionario.repository.ClienteRepository;
 
 @Service
@@ -13,5 +17,24 @@ public class ClienteService {
         this.clienteRepository = clienteRepository;
     }
 
+    public Cliente registrarCliente(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listarCliente(){
+        return clienteRepository.findAll();
+    }
+
+    public Cliente buscarPorId(Integer id){
+        return clienteRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente no encontrado"));
+    }
+
+    public Cliente actualizarCliente(Integer id, Cliente datos){
+        Cliente cliente = buscarPorId(id);
+        cliente.setNombre(datos.getNombre());
+        cliente.setDni(datos.getDni());
+        return clienteRepository.save(cliente);
+    }
 
 }
