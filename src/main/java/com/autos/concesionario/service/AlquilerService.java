80849daec03ddb16c990afd26946c26230e0d933
tellangelov <tell.angelov@gmail.com>
2026-09-24 18:31:21 -0500

    Update AlquilerService.java

--- src/main/java/com/autos/concesionario/service/AlquilerService.java
+++ src/main/java/com/autos/concesionario/service/AlquilerService.java
@@ -3,6 +3,7 @@ package com.autos.concesionario.service;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
@@ -17,16 +18,25 @@ public class AlquilerService {
     private final AlquilerRepository alquilerRepository;
     private final ClienteRepository clienteRepository;
     private final VehiculoRepository vehiculoRepository;
+    private final ClienteService clienteService;
+    private final VehiculoService vehiculoService;
 
     public AlquilerService(AlquilerRepository alquilerRepository,
                            ClienteRepository clienteRepository,
-                           VehiculoRepository vehiculoRepository) {
+                           VehiculoRepository vehiculoRepository, ClienteService clienteService, VehiculoService vehiculoService) {
         this.alquilerRepository = alquilerRepository;
         this.clienteRepository = clienteRepository;
         this.vehiculoRepository = vehiculoRepository;
+        this.clienteService = clienteService;
+        this.vehiculoService = vehiculoService;
     }
 
-    public Alquiler alquilar(Alquiler alquiler){
+    public Alquiler alquilar(Alquiler datos){
+        Alquiler alquiler = new Alquiler();
+        alquiler.setCliente(clienteService.buscarPorId(datos.getCliente().getId()));
+        alquiler.setVehiculo(vehiculoService.buscarPorId(datos.getVehiculo().getId()));
+        alquiler.setFechaInicio(datos.getFechaInicio());
+        alquiler.setPrecioDia(datos.getPrecioDia());
         return alquilerRepository.save(alquiler);
     }
 
@@ -36,21 +46,23 @@ public class AlquilerService {
 
     public Alquiler buscarPorId(Integer id){
         return alquilerRepository.findById(id)
-                .orElseThrow(() -> new HttpStatusCodeException(HttpStatus.NOT_FOUND, "Alquiler no encontrado") {
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Alquiler no encontrado") {
                 });
 
     }
 
     public Alquiler actualizarAlquiler(Integer id, Alquiler data){
         Alquiler alquiler = buscarPorId(id);
-        alquiler.setCliente(data.getCliente());
+        alquiler.setCliente(clienteService.buscarPorId(data.getCliente().getId()));
         alquiler.setFechaFin(data.getFechaFin());
         alquiler.setFechaInicio(data.getFechaInicio());
         alquiler.setPrecioDia(data.getPrecioDia());
-        alquiler.setVehiculo(data.getVehiculo());
+        alquiler.setVehiculo(vehiculoService.buscarPorId(data.getVehiculo().getId()));
         return alquilerRepository.save(alquiler);
     }
 
-    public Alquiler finalizarAlquiler(Integer id, Date)
+    public Alquiler finalizarAlquiler(Integer id, LocalDate fechaFin){
+        
+    }
 
 }
